fd174d7f68ec8b1756e4ad2dcdc4222a41329061
carlosapa <carlos_non86@msn.com>
2014-04-22 20:27:06 +0200

    update 22.04#20.27

--- wp-content/themes/larahache/gallery.php
+++ wp-content/themes/larahache/gallery.php
@@ -12,12 +12,32 @@
 		<h2 class="title js-scroll-noticias">Galería de imágenes</h2 class="title">
 	</div>
 	<div id="post-gallery">	
+		<h2 class="gallery_name">Asamblea programática</h2>
 		<div id="post-gallery-holder">	
-			<?php    
-		        $galleries = get_post_galleries_images($page);
-		        ?>
-		        <pre><?php print_r($galleries)?></pre>
-		    ?>
+		<?php
+		  $CP =  get_the_id();
+		?>
+		<?php query_posts('p=' . $CP . ''); ?>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post();    
+
+		 $args = array(
+		   'post_type' => 'attachment',
+		   'numberposts' => -1,
+		   'orderby'       => 'name',
+		   'post_status' => null,
+		   'post_parent' => $CP
+		   );
+
+		  $attachments = get_posts( $args );
+		     if ( $attachments ) {
+		        foreach ( $attachments as $attachment ) {
+					echo '<li><p>';
+		           	echo wp_get_attachment_image( $attachment->ID, array(746,500) );
+					echo '</p></li>';
+		          }
+		     }
+
+		 endwhile; endif; ?>
 		</div>	
 	</div><!-- end of feed -->
 </div>
