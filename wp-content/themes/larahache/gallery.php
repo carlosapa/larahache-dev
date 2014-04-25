eec00871f3773a284c28628803a0be86eb85a00e
carlosapa <carlos_non86@msn.com>
2014-04-25 19:09:31 +0200

    update 25.04#19.09

--- wp-content/themes/larahache/gallery.php
+++ wp-content/themes/larahache/gallery.php
@@ -25,6 +25,15 @@
 
 		?>
 		</div>	
+do. url parse -> button
+<a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fparse.com" target="_blank">
+ Share on Facebook
+</a>
+
+<a href="http://twitter.com/home?status=go+to+this+page+http://www.example.com/%23/page10" target="_blank">
+<br/>
+Parsear esto -> Share on Twitter
+</a>
 	</div><!-- end of feed -->
 	<script type="text/javascript">
 
--- wp-content/themes/larahache/slider-index-dev.php
+++ wp-content/themes/larahache/slider-index-dev.php
@@ -1,24 +1,49 @@
-<?php ?>
+<?php
+//get informations of template
+	$args = array(
+		'post_type' => 'slider',
+	);
+	$query = new WP_Query( $args );
+	$meta = get_post_meta($query->posts[0]->ID);
+	$sliders = array(
+		'slider-1' => array(
+			'post' => $query->posts[2],
+			'meta' => get_post_meta($query->posts[2]->ID),
+		),
+		'slider-2' => array(
+			'post' => $query->posts[1],
+			'meta' => get_post_meta($query->posts[1]->ID),
+		),
+		'slider-3' => array(
+			'post' => $query->posts[0],
+			'meta' => get_post_meta($query->posts[0]->ID)
+		)
+	);
+?>
+
 <a class="title js-scroll-bio-lara"></a><!-- Anchor for the Bio-Lara part -->
 <div id="featured">
 	<div id="slider-home-holder">
 		<ul id="slider-home">
-
 			<li>
 				<div id="slider-presentaciones" style="height:360px;">
 					<div class="pull-left-wide">
-						<img src="<?php bloginfo('template_directory');?>/img/lara.jpg" alt="bio-img" title="lara-img" height="250"/>
+						<?php 
+							//img load
+							$img_attr = wp_get_attachment_image_src($sliders['slider-1']['meta']['gallery'][0], 'full', 0);
+						?>
+						<img src="<?php echo $img_attr[0];?>" alt="bio-img" title="lara-img" height="250"/>
 						<div id="caption">
-							<span id="bio-name" class="bio-data">Lara Hernández García</span>
-							<span id="bio-name" class="bio-data">Candidata al Parlamento Europeo por Izquierda Unida</span>
+							<span id="bio-name" class="bio-data"><?php echo $sliders['slider-1']['meta']['nombre-lara'][0]; ?></span>
+							<span id="bio-name" class="bio-data"><?php echo $sliders['slider-1']['meta']['subnombre-lara'][0]; ?></span>
 						</div>
 					</div>
 					<div class="pull-right-thin">
 						<div id="bio-text">
 							<div class="data height-130">
 								<?php
-										$text = get_page(get_the_ID());
-										$content = apply_filters('the_content', $text->post_content);
+										$text = $sliders['slider-1']['meta']['texto-presentacion'][0];
+										$content = apply_filters('the_content', $text);
 										$content = str_replace(']]>', ']]&gt;', $content);
 										echo $content;
 								?>
@@ -34,33 +59,39 @@
 				<div id="slider-video" style="height:360px;">
 					<div class="pull-left-half">
 						<div id="video-cont">
-							<iframe width="530" height="370" src="//www.youtube.com/embed/UrBhw377elc" frameborder="0" allowfullscreen></iframe>
+							<iframe width="530" height="370" src="<?php echo $sliders['slider-2']['meta']['video-link'][0]; ?>" frameborder="0" allowfullscreen></iframe>
 						</div>
 					</div>
 					<div class="pull-right-half">
-						<div id="feed-title" class="video">Intervención en la Asamblea de Madrid</div>
+						<div id="feed-title" class="video"><?php echo $sliders['slider-2']['meta']['video-nombre'][0]; ?></div>
 						<div id="tiny-gallery">
 							<div id="feed-title" class="gallery">Otras imágenes</div>
 							<div id="tiny-gallery-images">
-								<div style="width:170px">
-									<img src="<?php bloginfo('template_directory');?>/img/lara.jpg" alt="bio-img" title="lara-img" height="100">
-								</div>
-								<div style="width:90px">
-									<img src="<?php bloginfo('template_directory');?>/img/lara.jpg" alt="bio-img" title="lara-img" height="100">
-								</div>
-								<div style="width:170px">
-									<img src="<?php bloginfo('template_directory');?>/img/lara.jpg" alt="bio-img" title="lara-img" height="100">
-								</div>
-								<div style="width:90px">
-									<img src="<?php bloginfo('template_directory');?>/img/lara.jpg" alt="bio-img" title="lara-img" height="100">
-								</div>
-								<div style="width:170px">
-									<img src="<?php bloginfo('template_directory');?>/img/lara.jpg" alt="bio-img" title="lara-img" height="100">
-								</div>
-								<div style="width:90px">
-									<img src="<?php bloginfo('template_directory');?>/img/lara.jpg" alt="bio-img" title="lara-img" height="100">
-								</div>
+								<?php
+									//feed gallery from easy gallery
+									$gallery_slider_2  = $sliders['slider-2']['meta']['_easy_image_gallery'][0];
+									$gallery_image_ids = explode(",", $gallery_slider_2);
+									for ($i = 0; $i < count($gallery_image_ids); $i++) {
+										$image_data[$i] = wp_get_attachment_image_src($gallery_image_ids[$i], 'full', 0);
+										$url[$i]        = $image_data[$i][0]; 
+										?>
+
+										<div id="tiny-image">
+											<img src="<?php echo $url[$i]?>" alt="bio-img" title="lara-img" height="80">
+										</div>
+
+										<?php
+									}							
+								?>
+
+								<a href=""><div id="tiny-image" class="ver-mas">
+									<span>Ver más</span>
+								</div></a>
+
 							</div>
+							<script type="text/javascript">
+
+							</script>
 						</div>
 					</div>
 				</div>
@@ -72,19 +103,52 @@
 						<div id="feed-title" class="acto">Próximo acto</div>
 						<div id="imagen-acto">
 							<?php 
+								//tomo objeto post y deserializo el id del post
+								$post_object = $sliders['slider-3']['meta']['proximo-acto'][0];
+								$post_object_ser = unserialize($post_object);
 
+								//tomo los datos meta del post y los atributos de la imagen
+								$post = get_post($post_object_ser[0]);
+								$meta = get_post_meta($post_object_ser[0]);
+								$img_attr = wp_get_attachment_image_src($meta['post-image_type'][0], 'full', 0);
+								//la url de la imagen es siempre el 0
+								$img_src = $img_attr[0];
 							?>
+							<img src="<?php echo $img_src;?>"/>
 						</div>
 					</div>
 					<div class="pull-right-wide">
+
+						<div id="feed-info" class="slider">
+							<div id="feed-header">
+								<div id="feed-title"><?php echo get_the_title($post->ID); ?></div>
+								<div id="feed-date">
+									<?php 
+										//es la única manera de tomar la fecha formateada de una fecha
+										echo mysql2date('d.m.y', $post->post_date); 
+									?>
+								</div>
+								<div id="feed-author"><?php echo get_formated_author($post->post_author); ?></div>		
+							</div>
+							<div id="feed-content"><?php echo content_to($post->ID); ?></div>
+							<div id="feed-readmore"><a href="<?php echo get_permalink($post->ID); ?>" target="_self">Ver más de este post</a></div>
+						</div>
+
 					</div>
 				</div>
 			</li>
-
 		</ul>
 	</div>
 </div>
 
+<pre>
+	<?php 
+
+
+	//print_r($meta);
+
+	?>
+</pre>
 <script type="text/javascript">
 	var slider = $('#slider-home');
 	slider.anythingSlider({
